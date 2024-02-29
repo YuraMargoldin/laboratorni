9d5d1c88fa2afe927e052e1839329ddb67308e21
MARGOLDIN\yurap <yurapetruhin0302006@gmail,com>
2024-02-29 15:40:16 +0200

    Pr1

---
+++ Solution1/Practic1/Practic1.cpp
@@ -0,0 +1,12 @@
+﻿// Practic1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout << "Hello world!";
+	cin >> a;
+
+}
